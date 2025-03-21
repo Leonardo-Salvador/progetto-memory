05089526222a5531e9a0ecea91d4f353db03611b
Leonardo-Salvador <leonardo.salvador.posta@gmail.com>
2025-03-21 15:09:37 +0100

    tolti <p> da App()>

--- src/App.jsx
+++ src/App.jsx
@@ -17,13 +17,8 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           shot fatti {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
